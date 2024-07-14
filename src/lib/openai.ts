46d85e432c40f2b8bee5510b9129b890aaf9d44e
aleena-joseph-86 <jaleena727@gmail.com>
2024-07-15 00:21:52 +0530

    changes were made to the folder structure

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -4,7 +4,7 @@ const openai = new OpenAI({
   apiKey: process.env['OPENAI_API_KEY'], 
 });
 
-
+export { openai };
 
 
 
--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import openai from '../../src/lib/openai';
+import { openai }  from '../../lib/openai';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
