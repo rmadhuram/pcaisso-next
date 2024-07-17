bbb172f6a83b51e41388b3a379cdaeae9de92da6
aleena-joseph-86 <jaleena727@gmail.com>
2024-07-17 14:13:34 +0530

    Navbar and input area is modified

--- src/app/navbar/Navbar.tsx
+++ src/app/navbar/Navbar.tsx
@@ -3,9 +3,7 @@ import "./Navbar.css";
 export default function Navbar()  {
   return (
     <div className="navbar">
-      <h3>
-        Pc<span style={{ color: "rgb(228, 113, 113)" }}>ai</span>sso
-      </h3>
+      <h3>Pc<span style={{ color: "rgb(228, 113, 113)" }}>ai</span>sso</h3>
     </div>
   );
 }
