ba5765c00e2ff3e68b0224213e5d73a0747307d3
aleena-joseph-86 <jaleena727@gmail.com>
2024-07-21 17:59:37 +0530

    primereact components such as splitter and radio buttons implemented. Three folder are removed.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./navbar/Navbar";
+import 'primereact/resources/themes/saga-blue/theme.css';  
+import 'primereact/resources/primereact.min.css';          
+import 'primeicons/primeicons.css';                        
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,110 @@
-import Fulldisplay from "./fulldisplay/Fulldisplay";
+"use client";
 
-export default function Home()  {
+import React, { useState } from "react";
+import { RadioButton, RadioButtonChangeEvent } from "primereact/radiobutton";
+import { Splitter, SplitterPanel } from "primereact/splitter";
+import "./page.scss";
+
+interface Category {
+  name: string;
+  key: string;
+}
+
+export default function Home() {
+  const categories = [
+    { name: "2D (Canvas)", key: "2D" },
+    { name: "SVG (Vector Graphics", key: "SVG" },
+    { name: "3D (Three.js)", key: "3D" },
+    { name: "d3", key: "d3" },
+  ];
+
+  const [selectedCategory, setSelectedCategory] = useState<Category>(
+    categories[1]
+  );
+
   return (
-    <main>
-      <div>
-        <Fulldisplay />
-      </div>
-    </main>
+    <Splitter layout="horizontal">
+      <SplitterPanel
+        className="flex align-items-center justify-content-center left"
+        size={30}
+      >
+        <div className="input-container">
+          <p>
+            Describe what you imagine , and the system will draw it for you!
+            This is powered by OpenAI APIs (ChatGPT).
+          </p>
+          <div className="choices">
+            <div className="card flex justify-content-center">
+              <div className="flex flex-column gap-3">
+                {categories.map((category) => {
+                  return (
+                    <div
+                      key={category.key}
+                      className="flex align-items-start"
+                      style={{
+                        fontSize: "14px",
+                        fontFamily: "Chivo Mono, monospace",
+                        marginBottom: "10px",
+                      }}
+                    >
+                      <span style={{ padding: "0.5rem", paddingLeft: "0px" }}>
+                        <RadioButton
+                          inputId={category.key}
+                          name="category"
+                          value={category}
+                          onChange={(e: RadioButtonChangeEvent) =>
+                            setSelectedCategory(e.value)
+                          }
+                          checked={selectedCategory.key === category.key}
+                        />
+                      </span>
+                      <label htmlFor={category.key} className="ml-2">
+                        {category.name}
+                      </label>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+          <div className="input-area">
+            <p>What do you want to draw?</p>
+            <textarea
+              name="inputtext"
+              id="inputtext"
+              className="text-area"
+            ></textarea>
+          </div>
+          <div className="submit">
+            <button className="submit-button" type="submit">
+              Generate
+            </button>
+          </div>
+        </div>
+      </SplitterPanel>
+      <SplitterPanel
+        className="flex align-items-center justify-content-center right"
+        size={70}
+      >
+        <div className="home">
+          <h1>
+            Pc<span>ai</span>sso
+          </h1>
+          <h2>AI Art + Code</h2>
+          <div className="cover-pages">
+            <div className="cover-3 cover"></div>
+            <div className="cover-1 cover"></div>
+            <div className="cover-2 cover"></div>
+          </div>
+          <p className="footer">
+            Made with 💖 by the <a href="#">students and friends</a> of{" "}
+            <a target="_blank" href="https://gct.ac.in/">
+              {" "}
+              GCT, Coimbatore
+            </a>
+          </p>
+        </div>
+      </SplitterPanel>
+    </Splitter>
   );
 }
