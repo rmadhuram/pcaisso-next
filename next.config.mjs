9fd2038b3a9e091066543e073266e70cfdd0867f
rmadhuram <rmadhuram@yahoo.com>
2024-07-26 21:01:33 +0530

    using redirect since exportPathMap is deprecated

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      '/': { page: '/home' }
-    }
-  },
+  redirects: async function () {
+    return [
+      {
+        source: '/',
+        // `source` type `String` - is the incoming request path pattern
+        destination: '/home',
+        // `destination` type `String` - is the path you want to route to
+        permanent: true,
+        // `permanent` type `Boolean` Only for Redirects
+        // `true` will use 308 to cache the redirect
+        // `false` will use 307 as temporary
+      },
+    ]
+  }
 };
 
 export default nextConfig;
