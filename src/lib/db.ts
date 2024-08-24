1da5848194af9bd9763b51872bbec12ca9c8b196
aleena-joseph-86 <jaleena727@gmail.com>
2024-08-24 15:47:55 +0530

    Implemented connection pooling.

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,16 +1,27 @@
 import mysql from "mysql2/promise";
 
-let connection: any;
+let pool: any;
 
 export default async function connectDB() {
-  if (!connection) {
-    connection = await mysql.createConnection({
+  try {
+    pool = mysql.createPool({
       host: process.env.MYSQL_HOST,
-      port: process.env.MYSQL_PORT ? parseInt(process.env.MYSQL_PORT) : undefined,
+      port: process.env.MYSQL_PORT
+        ? parseInt(process.env.MYSQL_PORT)
+        : undefined,
       user: process.env.MYSQL_USER,
       password: process.env.MYSQL_PASSWORD,
       database: process.env.MYSQL_DATABASE,
+      waitForConnections: true,
+      connectionLimit: 10,
+      maxIdle: 10,
+      idleTimeout: 60000,
+      queueLimit: 0,
+      enableKeepAlive: true,
+      keepAliveInitialDelay: 0,
     });
+  } catch (error) {
+    console.log(error);
   }
-  return connection;
+  return pool;
 }
