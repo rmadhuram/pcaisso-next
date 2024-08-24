90b0a9b28b126c9e324ff4000618184eaa6a254b
aleena-joseph-86 <jaleena727@gmail.com>
2024-08-24 21:52:57 +0530

    CopyToClipBoard is icluded in the codeViewer.

--- src/app/draw/results.tsx
+++ src/app/draw/results.tsx
@@ -1,8 +1,29 @@
 import { TabView, TabPanel } from "primereact/tabview";
 import styles from "./results.module.scss";
 import CodeWithLineNumbers from "@/app/components/CodeWithLineNumbers";
+import { useState } from "react";
+import copy from "clipboard-copy";
+import { Button } from "primereact/button";
 
-export default function Results({diagram, text, timetaken}: {diagram: string, text: string, timetaken: number}) {
+export default function Results({
+  diagram,
+  text,
+  timetaken,
+}: {
+  diagram: string;
+  text: string;
+  timetaken: number;
+}) {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const handleCopyClick = async () => {
+    try {
+      await copy(text);
+      setIsCopied(true);
+    } catch (error) {
+      console.error("Failed to copy text to clipboard", error);
+    }
+  };
 
   return (
     <div className={styles["results"]}>
@@ -16,8 +37,11 @@ export default function Results({diagram, text, timetaken}: {diagram: string, te
           </div>
         </TabPanel>
         <TabPanel header="Code">
+            <Button className="copyBtn" onClick={handleCopyClick}>
+              {isCopied ? "Copied!" : "Copy to Clipboard"}
+            </Button>
           <pre className="code-panel">
-            <CodeWithLineNumbers language="html" code={text}/>
+            <CodeWithLineNumbers language="html" code={text} />
           </pre>
         </TabPanel>
       </TabView>
