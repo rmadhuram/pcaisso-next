5579f31567de72b14e373a3b9b32d2db89bcf8ac
aleena-joseph-86 <jaleena727@gmail.com>
2024-08-25 22:27:29 +0530

    schema.sql is included.

---
+++ sql/schema.sql
@@ -0,0 +1,29 @@
+-- users table
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(50) DEFAULT NULL,
+  `name` varchar(128) DEFAULT NULL,
+  `created_time` timestamp NULL DEFAULT NULL,
+  `last_session_time` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- results table
+CREATE TABLE `results` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(36) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `type` enum('2D','SVG','3D','D3') DEFAULT NULL,
+  `description` varchar(256) DEFAULT NULL,
+  `prompt` text DEFAULT NULL,
+  `model` varchar(128) DEFAULT NULL,
+  `output` text DEFAULT NULL,
+  `thumbnail_url` varchar(1024) DEFAULT NULL,
+  `created_time` timestamp NULL DEFAULT NULL,
+  `time_taken` double DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_userId` (`user_id`),
+  CONSTRAINT `fk_userId` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
