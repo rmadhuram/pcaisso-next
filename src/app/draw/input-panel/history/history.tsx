a719baa97509a1d510c830e479da5c4fb59c6ca7
rmadhuram <rmadhuram@yahoo.com>
2024-08-30 00:11:22 +0530

    history item mocks

--- src/app/draw/input-panel/history/history.tsx
+++ src/app/draw/input-panel/history/history.tsx
@@ -1,7 +1,32 @@
+import styles from "./history.module.scss";
+
+function HistoryItem({
+  ago,
+  prompt,
+  liked,
+}: {
+  ago: string;
+  prompt: string;
+  liked: boolean;
+}) {
+  return (<div className="history-item">
+    <div className="top-section">
+      <div className="ago">{ago}</div>
+      {liked ? <i className="fa-solid fa-heart liked"></i> : <i className="fa-regular fa-heart"></i>}
+    </div>
+    <div className="bottom-section">
+      <div className="prompt">{prompt}</div>
+    </div>
+  </div>);
+}
+
 export default function History() {
   return (
-    <div>
-      <h1>History</h1>
+    <div className={styles.history}>
+      <HistoryItem ago="10 minutes ago" prompt="A beautiful landscape" liked={false} />
+      <HistoryItem ago="3 hours ago" prompt="A duck swimming in water" liked={true} />
+      <HistoryItem ago="2 days ago" prompt="Draw a building with a lot of windows and a lot of people. There are trees also in the background." liked={false} />
+
     </div>
   );
 }
