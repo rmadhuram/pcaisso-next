fca650eeace38f4b4e67e2d851883dbed53e6cfc
aleena-joseph-86 <jaleena727@gmail.com>
2024-09-03 02:24:29 +0530

    Top Navbar -selected color is changed to wheat.

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -1,8 +1,16 @@
+"use client";
 import styles from "./Navbar.module.scss";
 import Signin from "../signIn/Signin";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+  const pathname = usePathname();
+
+  const isActive = (href: any) => {
+    return pathname === href;
+  };
+
   return (
     <div className={styles["navbar"]}>
       <div className="left-side">
@@ -11,16 +19,33 @@ export default function Navbar() {
         </h3>
         <ul className="menu">
           <li>
-            <Link href="/">Home</Link>
+            <Link href="/" style={{ color: isActive("/") ? "wheat" : "white" }}>
+              Home
+            </Link>
           </li>
           <li>
-            <Link href="/draw">Explore</Link>
+            <Link
+              href="/draw/new"
+              style={{ color: isActive("/draw/new") ? "wheat" : "white" }}
+            >
+              Explore
+            </Link>
           </li>
           <li>
-            <Link href="/faq">FAQ</Link>
+            <Link
+              href="/faq"
+              style={{ color: isActive("/faq") ? "wheat" : "white" }}
+            >
+              FAQ
+            </Link>
           </li>
           <li>
-            <Link href="/about">About</Link>
+            <Link
+              href="/about"
+              style={{ color: isActive("/about") ? "wheat" : "white" }}
+            >
+              About
+            </Link>
           </li>
         </ul>
       </div>
