478f4e100b04e60404e363a8f80e65d104f3899f
aleena-joseph-86 <jaleena727@gmail.com>
2024-10-15 00:36:04 +0530

    schema.sql updated

--- sql/schema.sql
+++ sql/schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE `users` (
   `last_session_time` timestamp NULL DEFAULT NULL,
   `role` enum('user','admin') DEFAULT 'user',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- results table
 CREATE TABLE `results` (
@@ -16,9 +16,10 @@ CREATE TABLE `results` (
   `uuid` varchar(36) DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,
   `type` enum('2D','SVG','3D','D3') DEFAULT NULL,
-  `description` varchar(256) DEFAULT NULL,
+  `description` text DEFAULT NULL,
   `prompt` text DEFAULT NULL,
   `model` varchar(128) DEFAULT NULL,
+  `provider` varchar(45) DEFAULT NULL,
   `output` text DEFAULT NULL,
   `thumbnail_url` varchar(1024) DEFAULT NULL,
   `created_time` timestamp NULL DEFAULT NULL,
@@ -28,7 +29,10 @@ CREATE TABLE `results` (
   `liked` tinyint(1) DEFAULT NULL,
   `liked_time` timestamp NULL DEFAULT NULL,
   `status` enum('ACTIVE','DELETED') DEFAULT NULL,
+  `input_cost` double(2,2) DEFAULT NULL,
+  `output_cost` double(2,2) DEFAULT NULL,
+  `total_cost` double(2,2) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_userId` (`user_id`),
   CONSTRAINT `fk_userId` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=246 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=574 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
